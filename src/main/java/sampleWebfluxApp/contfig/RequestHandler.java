298af7e2ea2a6f7e0e0908751cf3072736bb5650
rej <Rayearth777@wp.pl>
2023-04-05 21:03:47 +0200

    add to Router functional endpoint MediaType.TEXT_EVENT_STREAM router/table/{input}/stream close #2

--- src/main/java/sampleWebfluxApp/contfig/RequestHandler.java
+++ src/main/java/sampleWebfluxApp/contfig/RequestHandler.java
@@ -1,6 +1,7 @@
 package sampleWebfluxApp.contfig;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -29,4 +30,13 @@ public Mono<ServerResponse>tableHandler(ServerRequest serverRequest){
 		Flux<Response> responseFlex = this.matchService.multiplicationTable(input);
 		return ServerResponse.ok().body(responseFlex, Response.class);		
 	}
+
+public Mono<ServerResponse>tableStreamHandler(ServerRequest serverRequest){
+	
+	int input = Integer.parseInt(serverRequest.pathVariable("input"));
+	Flux<Response> responseFlex = this.matchService.multiplicationTable(input);
+	return ServerResponse.ok()
+			.contentType(MediaType.TEXT_EVENT_STREAM)
+			.body(responseFlex, Response.class);		
+}
 }
