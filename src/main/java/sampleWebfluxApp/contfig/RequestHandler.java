cdcae58c3be4086ee8b50340264eb51b276ef772
rej <Rayearth777@wp.pl>
2023-04-05 20:46:10 +0200

    create RouterHandler witch two rest endpoints router/square/{input} router/table/{input} close #2

---
+++ src/main/java/sampleWebfluxApp/contfig/RequestHandler.java
@@ -0,0 +1,32 @@
+package sampleWebfluxApp.contfig;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import sampleWebfluxApp.dto.Response;
+import sampleWebfluxApp.service.ReactiveMathService;
+
+@Service
+public class RequestHandler {
+
+	@Autowired
+	private ReactiveMathService matchService;
+	
+	public Mono<ServerResponse>squareHandler(ServerRequest serverRequest){
+		
+		int input = Integer.parseInt(serverRequest.pathVariable("input"));
+		Mono<Response> responseMono = this.matchService.findSquare(input);
+		return ServerResponse.ok().body(responseMono, Response.class);		
+	}
+	
+public Mono<ServerResponse>tableHandler(ServerRequest serverRequest){
+		
+		int input = Integer.parseInt(serverRequest.pathVariable("input"));
+		Flux<Response> responseFlex = this.matchService.multiplicationTable(input);
+		return ServerResponse.ok().body(responseFlex, Response.class);		
+	}
+}
---
+++ src/main/java/sampleWebfluxApp/contfig/RouterConfig.java
@@ -0,0 +1,24 @@
+package sampleWebfluxApp.contfig;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
+public class RouterConfig {
+
+	@Autowired
+	private RequestHandler requestHandler;
+	
+	@Bean
+	public RouterFunction<ServerResponse> serverResponseRouterFunction(){
+		return RouterFunctions.route()
+				.GET("router/square/{input}", requestHandler::squareHandler)
+				.GET("router/table/{input}", requestHandler::tableHandler)
+				.build();
+				
+	}
+}
