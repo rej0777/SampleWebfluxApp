081f4138c43030c6528d5a2c01b859ac0ee4f764
rej <Rayearth777@wp.pl>
2023-04-20 20:10:52 +0200

    responseFlex @Test

---
+++ src/test/java/sampleWebfluxApp/GetMultiResponseTest.java
@@ -0,0 +1,33 @@
+package sampleWebfluxApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+import sampleWebfluxApp.dto.Response;
+
+public class GetMultiResponseTest extends BaseTest {
+
+	@Autowired
+	private WebClient webClient;
+	
+	
+	@Test
+	public void fluxTest() {
+		
+		Flux<Response> responseFlex= this.webClient
+		.get()
+		.uri("math/table/{input}", 5)
+		.retrieve()
+		.bodyToFlux(Response.class)
+		.doOnNext(System.out::println);
+		
+		StepVerifier.create(responseFlex)
+		.expectNextCount(10)
+		.verifyComplete();
+	}
+	
+}
